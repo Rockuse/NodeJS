ed75373f47c8c919136d8a0a5b42bce09ec87a15
Fahmi Muzakki <fahmirockman@gmail.com>
2022-11-03 16:59:23 +0700

    renamed:    NodeJS-Project/server.js -> NodeJS-Project/src/server/app.js 	renamed:    NodeJS-Project/server-planet.js -> NodeJS-Project/src/server/server-planet.js 	new file:   NodeJS-Project/src/server/server.js

--- NodeJS-Project/src/server/app.js
+++ NodeJS-Project/src/server/app.js
@@ -3,16 +3,11 @@ const routes = require('@src/routes');
 const express = require('express');
 const path = require('path');
 
-const PORT = process.env.PORT || 3002;
 const app = express();
-
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/site', express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
-
-app.listen(PORT, () => {
-  console.log('server is running');
-});
 
 module.exports = app;
