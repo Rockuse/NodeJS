f2ba36caec37509b58c9905fac644b49a060fc6e
Rockuse <fahmirockman@gmail.com>
2022-11-11 07:47:28 +0700

    modified:   backend/config/server.js 	modified:   backend/package-lock.json 	modified:   frontend/package-lock.json 	modified:   frontend/src/hooks/requests.js

--- backend/config/server.js
+++ backend/config/server.js
@@ -3,13 +3,12 @@ const http = require('http');
 const loadPlanetData = require('@src/planets/planet.models');
 const app = require('./app');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 async function start() {
   await loadPlanetData();
   server.listen(PORT, process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0', () => {
     console.log(`connected to ${process.env.PORT}`);
-    console.log(process.env);
   });
 }
 start();
