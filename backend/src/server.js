c80a072bc1898755f74928c512709f88795ad333
Fahmi Muzakki <fahmirockman@gmail.com>
2022-11-11 13:21:42 +0700

    modified:   backend/package.json 	renamed:    backend/config/app.js -> backend/src/app.js 	modified:   backend/src/server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,14 @@ require('dotenv').config();
 require('module-alias/register');
 const http = require('http');
 const loadPlanetData = require('@src/planets/planet.models');
-const app = require('@config/app');
+const app = require('@src/app');
 
 const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 async function start() {
   await loadPlanetData();
   server.listen(PORT, process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0', () => {
-    console.log(`connected to ${process.env.PORT}`);
+    console.log(`connected to ${process.env.LINK}`);
   });
 }
 start();
