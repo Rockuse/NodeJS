dce13af511cae9a226ef07e39dc8a96ab32c09e7
Fahmi Muzakki <fahmirockman@gmail.com>
2022-12-13 13:11:51 +0700

    add helmet

--- backend/config/middleware.js
+++ backend/config/middleware.js
@@ -1,10 +1,12 @@
 const cors = require('cors');
 const path = require('path');
+const helmet = require('helmet');
 
 module.exports = (app, express) => {
   app.use(cors());
   app.use(express.json());
   app.use(express.static(path.join(__dirname, '..', 'build')));
   app.use(express.urlencoded({ extended: false }));
+  app.use(helmet());
   return app
 };
