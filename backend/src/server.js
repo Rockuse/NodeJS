710330dc49d828ad6b5f76181c295fe6d5d011c5
Rockuse <fahmirockman@gmail.com>
2022-12-21 07:56:16 +0700

    modified:   backend/package-lock.json 	modified:   backend/src/server.js 	new file:   cert.pem 	modified:   frontend/package-lock.json 	new file:   key.pem

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,20 @@
 require('dotenv').config();
 require('module-alias/register');
 const mongoose = require('mongoose');
-const http = require('http');
+const https = require('https');
+const fs = require('fs')
+const {resolve} = require('path')
 const { loadPlanetData } = require('@src/planets/planet.models');
 const app = require('@src/app');
 
 const MONGODB_URL = 'mongodb+srv://rockuse-api:2UdeBlbxHSFlHMdn@rockuse-api.lbzpkht.mongodb.net/?retryWrites=true&w=majority';
 
 const PORT = process.env.PORT || 8000;
-const server = http.createServer(app);
+const server = https.createServer({
+  key:  fs.readFileSync(resolve(__dirname, '../../key.pem')),
+  cert: fs.readFileSync(resolve(__dirname, '../../cert.pem')),
+}, app);
+
 mongoose.connection.once('open', () => {
   console.log('MongoDB Connection is ready');
 });
